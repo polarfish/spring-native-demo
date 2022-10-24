40475b45fc5656ee340d6cef5bc769c975a59d07
Eugene Grybinnyk <eugeneg@backbase.com>
2022-10-24 11:11:13 +0200

    Removed NativeDemoHintsRegistrar.

--- src/main/java/com/example/nativedemo/NativeDemoApplication.java
+++ src/main/java/com/example/nativedemo/NativeDemoApplication.java
@@ -2,10 +2,8 @@ package com.example.nativedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(NativeDemoHintsRegistrar.class)
 public class NativeDemoApplication {
 
     public static void main(String[] args) {
